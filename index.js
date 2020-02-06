158c4efbd6c95c33269a2d034288b14ad746a4d4
Nathaly Cuya <57621005+natcuya@users.noreply.github.com>
2020-02-06 00:35:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 `use strict`;
 
-//const apikey = '8558c5ac1d7c7852d7cbbb122d60b949';
-//const searchURL = `https://ws.audioscrobbler.com/2.0/?api_key=$//{apikey}&format=json`;
-
 const apikey1 = `355095-capstone-3NMWSODY`;
 const searchUrl1 = `https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar`;
 
@@ -16,13 +13,14 @@ function displayResults2 (responseJson){
   console.log(responseJson);
   $(`#results-list`).empty();
   for (let i = 0; i<responseJson.Similar.Info.length; i++){
-  $('#results-list').append(`<div><h2> You searched: ${responseJson.Similar.Info[i].Name}</h2><h3>${responseJson.Similar.Info[i].wTeaser}</h3><h4><iframe width="560" height="315" src="${responseJson.Similar.Info[i].yUrl}frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></h4></div>`);}
+  $('#results-list').append(`<div id="artistSearch"><h2> You searched: ${responseJson.Similar.Info[i].Name}</h2><p>${responseJson.Similar.Info[i].wTeaser}</p><p><iframe width="560" height="315" src="${responseJson.Similar.Info[i].yUrl}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p></div>`
+  );}
   for (let i = 0; i<responseJson.Similar.Results.length; i++){
     $('#results-list').append(`
-    <div>
+    <div> Similar Artists:
       <h3>${responseJson.Similar.Results[i].Name}
-      </h3>
-      <h4><iframe width="560" height="315" src="${responseJson.Similar.Results[i].yUrl}frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></h4>
+      </h3><p>${responseJson.Similar.Results[i].wTeaser}</p>
+      <p><iframe width="560" height="315" src="${responseJson.Similar.Results[i].yUrl}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
     </div>`);
 };
 $('#results').removeClass('hidden');
@@ -31,10 +29,11 @@ $('#results').removeClass('hidden');
 function tasteDive (searchTerm) {
   const params = {
     q: searchTerm,
-    Type: "music",
+    type: `music`,
     limit: 3,
     info: 1,
     k: apikey1,
+    verbose: 1,
   };
 
   const queryString = formatQueryParams(params)
@@ -52,82 +51,9 @@ function tasteDive (searchTerm) {
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
+    
 }
 
-
-
-
-
-
-//function artistSearch (searchTerm) {
- // const params = {
-   // method: `artist.search`,
-   // limit: 4,
-   // artist: searchTerm};
-  //const queryString = formatQueryParams(params)
-  //const  url= searchURL + `&` + queryString;
-  //console.log(url);
-  //  fetch(url)
-  //  .then(response => {
-  //    if (response.ok) {
-   //     return response.json();}
-     // throw new Error(response.statusText);})
-    //.then(responseJson => displayResults(responseJson))
-    //.catch(err => {
-    //  $('#js-error-message').text(`Something went wrong: ${err.message}`);
-  //  });}
-
-//function getTopTracks(searchTerm){
- // const params = {
-  //  method: `artist.getTopTracks`,
-    //mbid: mbid1,
-  //  limit: 3,
-  //  artist: searchTerm
- // };
- // const queryString = formatQueryParams(params)
-//  const  url1 = searchURL + `&` + queryString;
-//  console.log(url1);
-//    fetch(url1)
- //   .then(response => {
- //     if (response.ok) {
-//     return response.json();
-//      }
- //     throw new Error(response.statusText);   })
-  //  .then(responseJson => displayResults1(responseJson))
-   // .catch(err => {
-   //   $('#js-error-message').text(`Something went wrong: ${err.message}`); });}
-//function getmbid(){
- // let mbid1 = (responseJson['results'][0]['mbid']);}
-
-//function displayResults (responseJson){
- // console.log(responseJson);
-  //$(`#results-list`).empty();
-  //for (let i = 0; i<responseJson.results.artistmatches.artist.length; i++){
-  //  $('#results-list').append(`
-  //  <div>
-   //   <h2 id="artistname"><a href=>"${responseJson.results.artistmatches.artist[i].name}
-   //   </a></h2>
-   //   <h3><a href=>"${responseJson.results.artistmatches.artist[i].url}"</a></h3>
-   //   <h4>${responseJson.results.artistmatches.artist[i].mbid}</h4>
-   // </div>`);};
-//$('#results').removeClass('hidden');};
-
-//function displayResults1(responseJson){
-  //console.log(responseJson);
- // for (let i = 0; i<responseJson.toptracks.track.length; i++){
- // $('#results-list').append(`
-  //    <div>
-  //      <h4>${responseJson.toptracks.track[i].name}
-   //    </h4>
-  //   </div>`);
-  //  }}
-//function artistTrack(){
-// $(`#artistName`).on(`click`, function (event) {
- //  $(`#results-list`).empty();
- //  $(`#results-list`).append(responseJson.toptracks.track[i].name);
- //}
-  // );
- // }
 
 function watchForm() {
   $('form').submit(event => {
@@ -136,7 +62,6 @@ function watchForm() {
   // artistSearch(searchTerm);
   // getTopTracks(searchTerm);
    tasteDive(searchTerm);
-  //getTopTracks(mbid1);
   });
   
 }
